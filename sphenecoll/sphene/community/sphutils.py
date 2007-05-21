c609382fbf4b74c0883fb75a0198bb372046be07
Herbert <herbert@poul.at>
2007-05-21 17:25:03 +0000

    fixed captchas by moving sphpermalink function to begin of module ;)

--- sphenecoll/sphene/community/sphutils.py
+++ sphenecoll/sphene/community/sphutils.py
@@ -1,5 +1,31 @@
 
 
+
+# Decorator. Takes a function that returns a tuple in this format:
+#     (viewname, viewargs, viewkwargs)
+#   Optionally takes a function which should either return an object with
+#     an attribute 'urlconf' or directly a python list which is used instead of
+#     settings.ROOT_URLCONF
+# Returns a function that calls urlresolvers.reverse() on that data, to return
+# the URL for those parameters.
+def sphpermalink(func, get_urlconf_func = None):
+    from django.core.urlresolvers import reverse
+    def inner(*args, **kwargs):
+        # Find urlconf ...
+        urlconf = None
+        if get_urlconf_func != None:
+            urlconf = get_urlconf_func()
+            if type(urlconf) != list:
+                # If type is no list, we assume it is a request object and
+                # look for a 'urlconf' attribute
+                urlconf = getattr(urlconf, 'urlconf', None)
+        
+        bits = func(*args, **kwargs)
+        viewname = bits[0]
+        return reverse(bits[0], urlconf, *bits[1:3])
+    return inner
+
+
 def get_fullusername(value):
     """ returns the full username of the given user - if defined
     (No HTML, just text) """
@@ -36,6 +62,7 @@ try:
     usecaptcha = True
 except:
     usecaptcha = False
+    print "No"
 
 from random import random
 
@@ -101,27 +128,3 @@ class CaptchaField(forms.fields.MultiValueField):
         return None
 
 
-
-# Decorator. Takes a function that returns a tuple in this format:
-#     (viewname, viewargs, viewkwargs)
-#   Optionally takes a function which should either return an object with
-#     an attribute 'urlconf' or directly a python list which is used instead of
-#     settings.ROOT_URLCONF
-# Returns a function that calls urlresolvers.reverse() on that data, to return
-# the URL for those parameters.
-def sphpermalink(func, get_urlconf_func = None):
-    from django.core.urlresolvers import reverse
-    def inner(*args, **kwargs):
-        # Find urlconf ...
-        urlconf = None
-        if get_urlconf_func != None:
-            urlconf = get_urlconf_func()
-            if type(urlconf) != list:
-                # If type is no list, we assume it is a request object and
-                # look for a 'urlconf' attribute
-                urlconf = getattr(urlconf, 'urlconf', None)
-        
-        bits = func(*args, **kwargs)
-        viewname = bits[0]
-        return reverse(bits[0], urlconf, *bits[1:3])
-    return inner
