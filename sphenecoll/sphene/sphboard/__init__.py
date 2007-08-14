f27da57133acc639f71d15f9141b6ef0eeccf866
Herbert <herbert@poul.at>
2007-08-14 21:42:03 +0000

    increased default view threshold from 10 to 100

--- sphenecoll/sphene/sphboard/__init__.py
+++ sphenecoll/sphene/sphboard/__init__.py
@@ -6,7 +6,7 @@ add_setting_defaults( {
     'board_count_views': True,
     'board_heat_days': 30,
     'board_heat_post_threshold': 10,
-    'board_heat_view_threshold': 10,
+    'board_heat_view_threshold': 100,
     'board_heat_calculator': 'sphene.sphboard.models.calculate_heat',
 
 
