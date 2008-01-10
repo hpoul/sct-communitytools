51636d271ad02ce03baab97ea6739e7444e69495
Herbert <herbert@poul.at>
2008-01-10 21:31:36 +0000

    fixed email notifications

--- sphenecoll/sphene/sphboard/models.py
+++ sphenecoll/sphene/sphboard/models.py
@@ -747,7 +747,8 @@ class Post(models.Model):
 
         if isnew and self.is_hidden != 0:
             self.set_new( True )
-        if not isnew and self.is_new() and self.is_hidden == 0:
+            isnew = false
+        elif not isnew and self.is_new() and self.is_hidden == 0:
             self.set_new( False)
             isnew = True
 
