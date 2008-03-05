e598defd85a7a6fbeef8f6743a7be1f20ca1a1ed
Herbert <herbert@poul.at>
2008-03-05 23:11:10 +0000

    committing compiled .mo files together with a script which would compile all messages.

---
+++ dist/scripts/compile-all-sph-messages.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+
+## Very simple script to compile all language files for all
+## SCT apps.
+
+#from django.bin.compile-messages import compile_messages
+c = __import__('django.bin.compile-messages', None, None, 'compile_messages')
+
+import os
+
+olddir = os.getcwd()
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+ROOT_PATH = os.path.join(ROOT_PATH,'../..')
+
+sphapps = ( 'community',
+            'sphboard',
+            #'sphwiki',
+            'sphlinklist',
+            )
+
+for app in sphapps:
+    os.chdir( os.path.join(ROOT_PATH, 'sphenecoll', 'sphene', app ) )
+    c.compile_messages()
+
+os.chdir( olddir )
+
