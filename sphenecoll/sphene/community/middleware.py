8f07130a6724841304d4664cdbb5e2fee868e33f
Herbert <herbert@poul.at>
2008-05-13 21:05:17 +0000

    added action to form tag.

--- sphenecoll/sphene/community/middleware.py
+++ sphenecoll/sphene/community/middleware.py
@@ -113,7 +113,7 @@ try:
 except ImportError:
     from django.utils._threading_local import local
 
-print "Executing module body."
+#print "Executing module body."
 _thread_locals = local()
 def get_current_request():
     return getattr(_thread_locals, 'request', None)
