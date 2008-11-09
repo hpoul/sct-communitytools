336f89d6eaf14a613ac74a4c76839cbcdd301f87
Herbert <herbert@poul.at>
2008-11-09 12:33:38 +0000

    fixed possible type error.

--- sphenecoll/sphene/sphquestions/templatetags/sphquestions_extras.py
+++ sphenecoll/sphene/sphquestions/templatetags/sphquestions_extras.py
@@ -14,7 +14,7 @@ register = template.Library()
 @register.inclusion_tag('sphene/sphquestions/_answervoting.html')
 def sphquestions_answervoting(qext, reply):
     user = get_current_user()
-    if not qext.is_question or reply.thread is None:
+    if not qext or not qext.is_question or reply.thread is None:
         return { 'hidden': True }
 
     # don't diplay answer voting if user is not logged in
