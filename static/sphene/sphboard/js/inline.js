ffb9cb2255e73a5ccf082054abcbec4e5ce8ff5a
Herbert <herbert@poul.at>
2009-02-06 20:42:31 +0000

    updated to the latest version of the wysiwyg editor..

--- static/sphene/sphboard/js/inline.js
+++ static/sphene/sphboard/js/inline.js
@@ -1,18 +1,19 @@
-function activateBBCodeEditor() {
   var t = document.getElementById('id_body');
   content = t.innerHTML;
   var p = t.parentNode.previousSibling;
   var i, n;
-  var a = new Array("doClick('bold')","doClick('italic')","doClick('underline')","doClick('RemoveFormat')","doLink()","doClick('unlink')","doImage()","doQC(1)","doQC(2)");
-  var alt = new Array("bold","italic","underline","clear","link","unlink","picture","quote","code");
-  var c = new Array("black","red","blue","green","orange","fuchsia","aqua","lime","maroon","purple","navy","teal");
+  var a = new Array("doClick('undo')","doClick('redo')","doClick('bold')","doClick('italic')","doClick('underline')","doClick('RemoveFormat')","doLink()","doClick('unlink')","doImage()","doQC(1)","doQC(2)");
+  var alt = new Array("undo","redo","bold","italic","underline","clear","link","unlink","picture","quote","code");
+  var c = new Array("black","red","blue","green","orange","fuchsia","aqua","lime","maroon","purple","teal");
 
   var str = "<table><tr>";
-  for(i=0;i<9;i++) {
+  for(i=0;i<11;i++) {
     str += '<td><div class="bn" onmouseover="switchBtn(this, 1)" onmouseout="switchBtn(this, 0)" onclick="' + a[i] + '" style="cursor:pointer;"><img src="'+editor_static+'/images/' + alt[i] + '.gif" title="' + alt[i] + '" width=16 height=16></div></td>';
+    if(i==1)
+      str += '<td><div style="border:1px solid #BBBBBB;border-style:inset;height:18px;"></div></td>';
   }
   str += '<td><div style="border:1px solid #BBBBBB;border-style:inset;height:18px;"></div></td>';
-  for(i=0;i<12;i++) {
+  for(i=0;i<11;i++) {
     str += '<td><div class="bn" onmouseover="switchBtn(this, 1)" onmouseout="switchBtn(this, 0)" onclick="doColor(\'' + c[i] + '\')" style="cursor:pointer;"><div style="width:16px;height:16px;background-color:' + c[i] + ';"></div></div></td>';
   }
   str += '</tr></table><iframe id="rte" style="border:1px solid grey;width:500px;height:300px"></iframe><input type="hidden" id="id_body" name="body"/>';
@@ -31,15 +32,23 @@ function activateBBCodeEditor() {
   }
   p.innerHTML = str + '</table></div>';
   
-  setTimeout('init()', 100);
-}
   function init() {
     ifm = document.getElementById("rte");
-    myeditor = ifm.contentWindow.document;
-    bbcode2html();
-    myeditor.open();
-    myeditor.write('<html><head><link rel="stylesheet" href="'+editor_static+'/styles/editor.css"/></head><body>' + content + '</body></html>');
-    myeditor.close();
-    myeditor.designMode = "on";
+    if(ifm) {
+      myeditor = ifm.contentWindow.document;
+      bbcode2html();
+      myeditor.open();
+      myeditor.write('<html><head><link rel="stylesheet" href="'+editor_static+'/styles/editor.css"/></head><body>' + content + '</body></html>');
+      myeditor.close();
+      myeditor.designMode = "on";
+      if (myeditor.attachEvent){
+        myeditor.attachEvent("onkeypress", kp);
+      }else if(myeditor.addEventListener){
+        myeditor.addEventListener("keypress",kp,true);
+      }else{
+        myeditor["onkeypress"] = kp;
+      }
+    }else
+      setTimeout('init()', 100);
   }
-activateBBCodeEditor();
+  setTimeout('init()', 100);
