b980baebcdbda076d75cf18fb9f43510aee18d2e
Herbert <herbert@poul.at>
2009-02-11 21:06:27 +0000

    slightly improved archive template tag by adding counts of posts for each month.

--- sphenecoll/sphene/sphblog/templatetags/sphblog_extras.py
+++ sphenecoll/sphene/sphblog/templatetags/sphblog_extras.py
@@ -3,7 +3,9 @@ import datetime
 from django import template
 from django.template.context import Context
 
-from sphene.sphblog.views import get_posts_queryset
+from sphene.community.middleware import get_current_group
+
+from sphene.sphblog.views import get_posts_queryset, get_board_categories
 
 register = template.Library()
 
@@ -17,17 +19,25 @@ def sphblog_showblogpost(context, post):
     retctx.update(ret)
     return retctx
 
-@register.inclusion_tag('sphene/sphblog/_showarchivelinks.html', takes_context = True)
-def show_archive(context):
-    # There has to be a better way of getting the posts
-    # which does not involve passing data in the context
-    threads = get_posts_queryset(context['group'], context['categories'])
+@register.inclusion_tag('sphene/sphblog/_showarchivelinks.html')
+def show_archive(categories = None):
+    group = get_current_group()
+    if categories is None:
+        categories = get_board_categories(group)
+    threads = get_posts_queryset(group, categories)
     # Get dates
     threads = threads.values('postdate')
-    # Make unique (year, month) tuple list
-    months = list(set([(t['postdate'].year, t['postdate'].month) for t in threads]))
+    # Count posts in (year, month) tuples
+    months_dict = dict()
+    for t in threads:
+        tmp = (t['postdate'].year, t['postdate'].month)
+        months_dict[tmp] = months_dict.get(tmp, 0) + 1
+
     # Sort it by year and then by month, in increasing order (last month first)
-    months = sorted(months, lambda a, b: cmp(a[0], b[0]) or cmp(a[1], b[1]))
+    months = sorted(months_dict.iteritems(), \
+                        lambda a, b: \
+                        cmp(a[0][0], b[0][0]) \
+                        or cmp(a[0][1], b[0][1]))
     months.reverse()
     return {'links': months}
 
