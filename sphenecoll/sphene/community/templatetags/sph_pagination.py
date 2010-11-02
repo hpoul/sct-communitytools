19554749b206b03935fbc450b50fad42cff3af3f
pigletto <pigletto>
2010-11-02 13:02:37 +0000

    improve pagination to support slicing of pages list

--- sphenecoll/sphene/community/templatetags/sph_pagination.py
+++ sphenecoll/sphene/community/templatetags/sph_pagination.py
@@ -2,16 +2,54 @@ from django import template
     
 register = template.Library()
 
+def define_page_range(page, pages):
+    window = 6
+    page_range = range( 1, pages+1 )
 
+    pg = 1
+    if page != -1:
+        pg = page
+
+    wnd_start = page_range[:pg]
+    wnd_end = page_range[pg:]
+
+    end_correction = window/2
+    if len(wnd_start) <= window/2:
+        end_correction = window/2 + (window/2-len(wnd_start)) + 1
+
+    start_correction = window/2 + 1
+    if len(wnd_end) <= window/2:
+        start_correction = window/2 + (window/2-len(wnd_end)) + 1
+
+    out = wnd_start[-start_correction:]
+    out = out + wnd_end[:end_correction]
+    return out
 
 @register.inclusion_tag('sphene/community/_pagination.html', takes_context=True)
-def sph_pagination(context, pages, page, url = '', getparam = 'page'):
+def sph_pagination(context, pages, page, url = '', getparam = 'page', compress=False):
     has_next = page < pages
     has_prev = page > 1
     if page == -1:
         has_next = has_prev = False
 
-    to_return = {'page_range': range( 1, pages+1 ),
+
+    if compress:
+        page_range = define_page_range(page, pages)
+
+        first = 1
+        last = pages
+
+        if first in page_range:
+            first = None
+
+        if last in page_range:
+            last = None
+    else:
+        page_range = range( 1, pages+1 )
+        first = None
+        last = None
+
+    to_return = {'page_range': page_range,
                  'page': page,
                  'pages': pages,
                  'has_next': has_next,
@@ -20,6 +58,8 @@ def sph_pagination(context, pages, page, url = '', getparam = 'page'):
                  'prev': page - 1,
                  'url': url,
                  'getparam': getparam,
+                 'first_page':first,
+                 'last_page':last
                  }
 
     if 'request' in context:
