6ed0078108c435746d4e208e80bb2400f136ea8e
Pigletto <pigletto@gmail.com>
2013-06-05 08:02:19 +0200

    do not show signature for spammers

--- sphenecoll/sphene/sphboard/utils.py
+++ sphenecoll/sphene/sphboard/utils.py
@@ -2,10 +2,10 @@ from django.contrib.auth.models import User
 
 from sphene.community import get_sph_setting
 from sphene.community.middleware import get_current_group
-from sphene.sphboard.models import UserPostCount
 
 
 def is_spammer(user_id):
+    from sphene.sphboard.models import UserPostCount
     apply_spammer_limits = False
     try:
         upc = UserPostCount.objects.get_post_count(User.objects.get(pk=user_id), get_current_group())
