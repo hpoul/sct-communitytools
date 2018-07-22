fd29a9ac4e5e41e296359e1c340f9abfb2868516
Herbert Poul <herbert@poul.at>
2018-07-23 01:15:18 +0200

    fix maintenance

--- sphenecoll/sphene/community/management/commands/sph_maintenance.py
+++ sphenecoll/sphene/community/management/commands/sph_maintenance.py
@@ -1,13 +1,14 @@
 
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 from sphene.community.signals import trigger_maintenance
 
-class Command(NoArgsCommand):
+
+class Command(BaseCommand):
     help = "Runs SCT maintenance tasks."
 
     requires_model_validation = True
 
-    def handle_noargs(self, **options):
+    def handle(self, **options):
         trigger_maintenance()
 
